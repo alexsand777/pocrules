8c9d37fa67d646ccc3cdce801a3aa30b7970f764
salexraj <salexraj@yahoo.com>
2023-06-08 19:31:23 -0700

    initial

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "cd43f5d8-af31-4a96-8aed-98ca93dc5c23";
+private static final String ID = "8e420416-97a6-420a-a07c-4c135cc4aae4";
 }
